9ff6944230c77109d9466779dfa8077137b97106
dani <danibayer2003@yahoo.com>
2026-05-18 15:33:58 +0200

    JS completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector(".btn");
-const quotes = document.querySelector(".quotes");
-const authors = document.querySelector(".authors");
+const quote = document.querySelector(".quote");
+const person = document.querySelector(".person");
 
 const quotes = [
   {
@@ -185,3 +185,11 @@ const quotes = [
     person: "Will Rogers",
   },
 ];
+
+btn.addEventListener("click", newQuote);
+
+function newQuote() {
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  quote.innerText = quotes[randomNumber].quote;
+  person.innerText = quotes[randomNumber].person;
+}
